92e4fbde563d49cf696e00410a6d65a40774fc87
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-08-04 11:25:41 -0700

    added controller and model test passing

--- __tests__/tvshows.test.js
+++ __tests__/tvshows.test.js
@@ -7,16 +7,30 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('#GET /tv_shows returns list of shows', async () => {
-    const resp = await request(app).get('/tv_shows');
+  it('#GET /tvshows returns list of shows', async () => {
+    const resp = await request(app).get('/tvshows');
     expect(resp.status).toBe(200);
     expect(resp.body).toEqual([
       {
-        id: 1,
+        id: '1',
         name: 'The Sopranos',
-        seasons: '6',
-        episodes: '86',
+        seasons: 6,
+        episodes: 86,
         years: '1999-2007',
+      },
+      {
+        id: '2',
+        name: 'Trailer Park Boys',
+        seasons: 13,
+        episodes: 115,
+        years: '2001-2008, 2014-Present',
+      },      
+      {
+        id: '3',
+        name: 'Everybody Loves Raymond',
+        seasons: 9,
+        episodes: 210,
+        years: '1996-2005',
       },
     ]);
   });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,33 +1,33 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS tv_shows
+DROP TABLE IF EXISTS tvshows;
 
-CREATE TABLE tv_shows (
+CREATE TABLE tvshows (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR NOT NULL,
     seasons INT NOT NULL,
     episodes INT NOT NULL,
-    years INT NOT NULL
+    years VARCHAR NOT NULL
 );
 
 INSERT INTO
-    tv_shows (name, seasons, episodes, years)
+    tvshows (name, seasons, episodes, years)
 VALUES
     (
         'The Sopranos',
-        '6',
-        '86',
+        6,
+        86,
         '1999-2007'
     ),
     (
         'Trailer Park Boys',
-        '13',
-        '115',
+        13,
+        115,
         '2001-2008, 2014-Present'
     ),
     (
         'Everybody Loves Raymond',
-        '9',
-        '210',
-        '1996 - 2005'
+        9,
+        210,
+        '1996-2005'
     );
